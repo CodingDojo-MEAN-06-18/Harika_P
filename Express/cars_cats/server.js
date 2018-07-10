4b49dc4beffe6dd4ff57d896d9ca9b2164556ab9
harik <pharika31@gmail.com>
2018-07-10 00:24:25 -0700

    pushing changes to cars and cats

--- Express/cars_cats/server.js
+++ Express/cars_cats/server.js
@@ -3,15 +3,6 @@ var app = express();
 app.use(express.static(__dirname + "/static"));
 console.log(__dirname);
 
-app.get('/',function (request, response) {
-  response.sendFile('index.html');
-})
-app.get('/cars',function (request, response) {
-  response.sendFile(__dirname + '/static' + '/cars.html');
-})
-app.get('/cats',function (request, response) {
-  response.sendFile(__dirname + '/static' + '/cats.html');
-})
 app.listen(8000, function() {
   console.log("listening on port 8000");
 })
