ae9e75d4bc2a709dfbdce924e672798670e8e946
harik <pharika31@gmail.com>
2018-07-12 12:53:00 -0700

    Survey assmnt

---
+++ Express/survey/server.js
@@ -0,0 +1,32 @@
+var express = require("express");
+var path = require("path");
+var session = require("express-session");
+var app = express();
+var bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static(path.join(__dirname, "./static")));
+app.set('views', path.join(__dirname, './views'));
+app.set('view engine', 'ejs');
+app.use(session({
+  secret: 'keyboardkitteh',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { maxAge: 60000 }
+}))
+app.get('/', function(req, res) {
+ res.render("index");
+})
+
+app.post('/result',function(req, res){
+  console.log("POST DATA \n\n", req.body)
+  var form_data = req.body;
+  res.render("result",{data: form_data});
+
+})
+
+
+app.listen(8000, function() {
+ console.log("listening on port 8000");
+});
